e4cb00df80b2f141c8b58c4527c64137883d7bc4
rafaelalvarez702 <rafaelalvarez702@outlook.es>
2023-11-01 17:08:17 -0300

    a

--- main.py
+++ main.py
@@ -27,15 +27,33 @@ def pagppal():
 #FUNCION 1
 @app.get( "/PlayTimeGenre/{genero}")
 async def PlayTimeGenre( genero : str ):
+    
+    '''Esta funcion debe devolver año con mas horas jugadas para dicho género.
+    usando como parametro el genero.
+    Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}'''
  
     data= df_PlayTimeGenre[df_PlayTimeGenre['genres'] == genero]
     # Encontrar el año con más horas jugadas
     año_horas = data.groupby('Año')['playtime_forever'].sum().idxmax(0)
-    
-   
-    
     return {f"Año de lanzamiento con más horas jugadas para {genero}": año_horas}
 
+#FUNCION 2
+@app.get( "/UserForGenre/{genero}")
+async def UserForGenre( genero : str ):
 
+    '''
+    Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
+    Ejemplo de retorno: {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, 
+    "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
+    '''
+    data= df_UserForGenre[df_UserForGenre['genres'] == genero]
+    usuarios_horas = data.groupby('user_id')['playtime_forever'].sum().idxmax(0)
+    horas_por_año = data.groupby('Año')['playtime_forever'].sum().reset_index()
+    resultado = {
+        "Usuario con más horas jugadas para " + genero: usuarios_horas,
+        "Horas jugadas": [{"Año": int(row['Año']), "Horas": int(row['playtime_forever']/60)} for index, row in horas_por_año.iterrows()]
+    }
+    
+    return resultado
 
 
